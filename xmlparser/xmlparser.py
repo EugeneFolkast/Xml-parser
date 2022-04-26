5777e2f8b958acb7ab20172a9f70dc633660c6dd
Evgenii Karelin <eugenefolkast@gmail.com>
2022-04-26 18:09:52 +0300

    monochrome
    monochrome image with objects in different type of gray

--- xmlparser/xmlparser.py
+++ xmlparser/xmlparser.py
@@ -30,6 +30,20 @@ class Xmlparser:
 
         self.img = cv2.imread('D:/gits/Unettestproj/source/sourceimg/i (1).jpg')
 
+        self.CLASSES = {'nothing':0 ,'armchair':1, 'chair':2,
+                        'coach':3, 'dressers_and_cabinets_with_drawers':4,
+                        'floor_cabinet':5, 'mirror':6,
+                        'office_chair':7, 'shelving_and_bookcases':8,
+                        'table_lamps':9, 'wall_shelves':10, 'table_lmps':9,
+                        'wall_shelves_for_books':11, 'writing_desk':12, 'table':13}
+
+        self.COLORS = {0:(0, 255, 0), 1:(128, 255, 0),
+                       2:(196, 255, 0), 3:(255, 255, 0),
+                       4:(255, 188, 0), 5:(255, 119, 0),
+                       6:(255, 150, 0), 7:(255, 255, 0),
+                       8:(255, 50, 0), 9:(255, 80, 0),
+                       10:(255, 130, 0), 11:(255, 170, 0),
+                       12:(255, 200, 0), 13:(255, 255, 0)}
 
     def printRootNode(self):
         """
@@ -42,7 +56,6 @@ class Xmlparser:
         :return:
         """
         i = 0
-        j = 0
 
         for element in self.files:
             # Образуем дерево тэгов из xml файлов
@@ -58,10 +71,21 @@ class Xmlparser:
             if (buf3) != buf4:
                 continue
 
+            self.img = cv2.imread('D:/gits/Unettestproj/source/sourceimg/' + self.imgFilse[i])
+
+            rows, cols = self.img.shape[:2]
+
+            contours1 = [numpy.array([[0, 0],
+                                     [cols, 0],
+                                    [cols, rows],
+                                     [0, rows]])]
+
+            cv2.fillPoly(self.img, contours1, (0, 0, 0))
 
             for tag in self.tree.findall('object'):
-                # Получаем изображение для обработки opencv в соответствии с именем подходящего xml файла
-                self.img = cv2.imread('D:/gits/Unettestproj/source/sourceimg/' + self.imgFilse[i], cv2.IMREAD_GRAYSCALE)
+
+                if len(tag[0].text) < 5:
+                    continue
 
                 objectName = tag[0].text
 
@@ -72,31 +96,30 @@ class Xmlparser:
 
                 self.dataList.append(self.dataListForOne.copy())
 
-                cv2.rectangle(self.img, (int(self.dataListForOne[1]), int(self.dataListForOne[2])),
-                                        (int(self.dataListForOne[3]), int(self.dataListForOne[4])), 1)
+                colorClass = self.CLASSES[objectName]
+                objectColor = self.COLORS[colorClass]
 
-                stencil = numpy.zeros(self.img.shape).astype(self.img.dtype)
+                cv2.rectangle(self.img, (int(self.dataListForOne[1]), int(self.dataListForOne[2])),
+                                        (int(self.dataListForOne[3]), int(self.dataListForOne[4])), objectColor, 1)
+
+
                 contours = [numpy.array([[int(self.dataListForOne[1]), int(self.dataListForOne[2])],
                                          [int(self.dataListForOne[3]), int(self.dataListForOne[2])],
                                          [int(self.dataListForOne[3]), int(self.dataListForOne[4])],
                                          [int(self.dataListForOne[1]), int(self.dataListForOne[4])]])]
 
-                cv2.fillPoly(stencil, contours, color=[255, 255, 255])
-                result = cv2.bitwise_and(self.img, stencil)
-
-                result = result[int(self.dataListForOne[2]):int(self.dataListForOne[4]),
-                                int(self.dataListForOne[1]):int(self.dataListForOne[3])]
-
-                result = cv2.resize(result, (512, 512))
-                # Сохраняем обработанное изображение
-                cv2.imwrite('D:/gits/Unettestproj/source/proceedimgdir/data/' + self.imgFilse[i], result)
-
-                j += 1
+                cv2.fillPoly(self.img, contours, objectColor)
 
                 self.dataListForOne.clear()
 
             i += 1
-            j = 0
+
+            self.img = cv2.resize(self.img, (512, 512))
+
+            self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2GRAY)
+
+            # Сохраняем обработанное изображение
+            cv2.imwrite('D:/gits/Unettestproj/source/proceedimgdir/data/' + self.imgFilse[i], self.img)
 
 
 if __name__ == '__main__':
